8c877cca119087ce461eb440d0f5aaf53576feb3
Aysa Dzhalaeva <adzhalaeva@gmail.com>
2020-07-09 09:32:00 -0400

    add CreateAChannel.java

---
+++ src/day_1/CreateAChannel.java
@@ -0,0 +1,11 @@
+package day_1;
+
+public class CreateAChannel {
+    public static void main(String[] args) {
+
+        System.out.println("Spring 2020");
+        System.out.println("Batch 18");
+        System.out.println("Group online-11");
+        System.out.println("Cybernators");
+    }
+}
